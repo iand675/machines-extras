eb5abe95e5e3de781959f254f40ad57443b84b78
Ian Duncan <ian@iankduncan.com>
2015-07-13 11:32:40 -0400

    Add machines-vector

---
+++ machines-vector/src/Data/Vector/Machine.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Data.Vector.Machine where
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans
+import Data.Machine
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as M
+
+buffered :: forall m a. MonadIO m => Int -> ProcessT m a (V.Vector a)
+buffered c = repeatedly $ do
+  v <- liftIO $ M.new c
+  build v 0
+  where
+    end = c
+    build v i = if i == end
+      then liftIO (V.unsafeFreeze v) >>= \fv -> yield fv
+      else do
+        x <- await <|> do
+          fv <- liftIO $ V.unsafeFreeze $ M.unsafeSlice 0 (i - 1) v
+          yield fv
+          stop
+        liftIO $ M.write v i x
+        build v $! i + 1
+
